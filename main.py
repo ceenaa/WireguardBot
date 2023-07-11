14b8984248dbd314416c50f3cc92f088e021ca07
sina <sina.moradi2003@yahoo.com>
2023-07-12 00:13:08 +0330

    fix: bug in reload

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def send_usage(message):
         connection.close()
         bot.send_message(message.chat.id,
                          "Name: " + name + "\nStart time: " + start_time + " : " + str(
-                             functions.total_days()) + "days" + "\nUsage: " + usage + " Gib")
+                             functions.total_days()) + "days" + "\nUsage: " + str(usage) + " Gib")
     except Exception as err:
         bot.send_message(message.chat.id, type(err).__name__ + " " + str(err))
 
