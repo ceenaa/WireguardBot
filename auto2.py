b34de2c545623d0643ad24a1fe338f75cdb8ad61
sina <sina.moradi2003@yahoo.com>
2023-07-15 01:20:24 +0330

    feat: Pausing user after 30 days

---
+++ auto2.py
@@ -0,0 +1,19 @@
+import gspread
+
+import functions
+import sheet
+
+sa = gspread.service_account(filename='keys.json')
+sh = sa.open_by_key(sheet.sheet_id)
+
+wks = sh.worksheet("Sheet1")
+
+conf_names = wks.col_values(3)
+days = wks.col_values(5)
+status = wks.col_values(6)
+
+for i in range(len(conf_names)):
+    if conf_names[i] == "conf_name":
+        continue
+    if days[i] == "31" and status[i] == "1":
+        functions.pause_user(conf_names[i])
