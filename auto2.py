2e27af7e836933c54e127029c9e85c5607eab753
sina <sina.moradi2003@yahoo.com>
2023-07-15 01:44:37 +0330

    feat: Pausing user after 30 days

--- auto2.py
+++ auto2.py
@@ -1,3 +1,5 @@
+import time
+import sched
 import gspread
 
 import functions
@@ -5,15 +7,24 @@ import sheet
 
 sa = gspread.service_account(filename='keys.json')
 sh = sa.open_by_key(sheet.sheet_id)
-
 wks = sh.worksheet("Sheet1")
+delay_time = 60 * 60 * 24
 
-conf_names = wks.col_values(3)
-days = wks.col_values(5)
-status = wks.col_values(6)
 
-for i in range(len(conf_names)):
-    if conf_names[i] == "conf_name":
-        continue
-    if days[i] == "31" and status[i] == "1":
-        functions.pause_user(conf_names[i])
+def auto(scheduler):
+    scheduler.enter(delay_time, 1, auto, (scheduler,))
+    conf_names = wks.col_values(3)
+    days = wks.col_values(5)
+    status = wks.col_values(6)
+
+    for i in range(len(conf_names)):
+        if conf_names[i] == "conf_name":
+            continue
+        if days[i] == "31" and status[i] == "1":
+            functions.pause_user(conf_names[i])
+
+
+def main():
+    my_scheduler = sched.scheduler(time.time, time.sleep)
+    my_scheduler.enter(1, 1, auto, (my_scheduler,))
+    my_scheduler.run()
